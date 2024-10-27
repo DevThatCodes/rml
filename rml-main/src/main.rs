43b9b4451045e5a4ca7f506817484a817789d6c1
DevThatCodes <cupcooklinux@gmail.com>
2024-10-27 12:55:52 -0400

    added parse_from_string

--- rml-main/src/main.rs
+++ rml-main/src/main.rs
@@ -1,5 +1,7 @@
+use std::fs;
+
 use rml::*;
 
 fn main() {
-    println!("{:#?}", RmlElement::default());
+    println!("try parse file:\n{:#?}", parse_from_string(fs::read_to_string("test.rml").unwrap()))
 }
--- rml/src/lib.rs
+++ rml/src/lib.rs
@@ -1,10 +1,12 @@
-#[derive(Debug, PartialEq)]
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Clone)]
 pub struct RmlTag {
     pub name: String,
     pub value: String
 }
     
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct RmlElement {
     pub name: String,
     pub tags: Vec<RmlTag>,
@@ -31,6 +33,15 @@ impl Default for RmlTag {
 
 // implement functions for RmlElement
 impl RmlElement {
+    // builder
+    pub fn new(name: String) -> Self {
+        RmlElement {
+            name,
+            tags: Vec::new(),
+            children: Vec::new()
+        }
+    }
+
     // returns a reference to the children field
     pub fn get_children(&self) -> Vec<&RmlElement> {
         let mut output : Vec<&RmlElement> = Vec::new();
@@ -73,6 +84,26 @@ impl RmlTag {
     }
 }
 
+pub fn is_start_tag(tag: String) -> bool {
+    !tag.contains("!") && is_tag(tag)
+}
+
+pub fn is_end_tag(tag: String) -> bool {
+    !is_start_tag(tag)
+}
+
+pub fn is_tag(tag: String) -> bool {
+    tag.contains("[") && tag.contains("]")
+}
+
+pub fn is_rmltag(tag: String) -> bool {
+    tag.contains("=")
+}
+
+pub fn get_tag_name(tag: String) -> String {
+    tag.replace("[", "").replace("]", "").replace("!", "").to_string()
+}
+
 // TODO: 2, 3.
 // DONE: 1.
 // 1. Figure out the algorithm to do it.
@@ -88,8 +119,92 @@ impl RmlTag {
 //
 // Pseudocode:
 // parse_from_string(string data) {
-//  
+//  content = parse_to_vec(data)
+//  scope = new vector
+//  current_parent = RmlElement
+//  child_hashmap = new hashmap()
+//  for thing in content do:
+//      if is_start_tag(thing) do:
+//          scope.add(thing)
+//          current_parent = new RmlElement(thing)
+//      else if is_end_tag(thing) do:
+//          scope.remove(thing)
+//          if not scope.is_empty() do:
+//              child_hashmap.key(scope.last()).add(thing)
+//          else:
+//              return current_parent
 // }
+
+fn parse_to_vec(data: String) -> Vec<String> {
+    let mut output : Vec<String> = Vec::new();
+    let mut substring = String::from("");
+    let mut in_tag = false;
+    data.chars().for_each(|char| {
+        if char == '[' {
+            if !in_tag {
+                in_tag = true
+            } else if !substring.is_empty() {
+                output.push(substring.clone());
+                substring.clear();
+            }
+        }
+        substring += &char.to_string();
+        if char == ']' {
+            output.push(substring.clone());
+            substring.clear();
+        }
+    });
+    output
+}
+
+pub fn parse_tag_from_string(data: String) -> RmlTag {
+    RmlTag { name: data.split("=").next().unwrap().to_string(), value: data.split("=").nth(1).unwrap().to_string() }
+}
+
+fn get_index<T: PartialEq>(vector: Vec<T>, value: T) -> usize {
+    vector.iter().position(|element| *element==value).unwrap()
+}
+
+pub fn parse_from_string(data: String) -> RmlElement {
+    let content = parse_to_vec(data);
+    let mut scope : Vec<String> = Vec::new();
+    let mut child_hashmap : HashMap<String, Vec<RmlElement>> = HashMap::new();
+    let mut current_parent : Vec<RmlElement> = Vec::new();
+    let mut current_parent_names : Vec<String> = Vec::new();
+    content.iter().for_each(|tag_or_content_in| {
+        let mut tag_or_content = tag_or_content_in.split(" ").next().unwrap().to_string();
+        let tags: Vec<String> = get_tag_name(tag_or_content_in.to_string()).split(" ").filter(|tag| is_rmltag(tag.to_string())).map(|tag| tag.to_string()).collect::<Vec<_>>();
+        if tag_or_content_in.contains("]"){
+            tag_or_content += "]";
+        }
+        if is_tag(tag_or_content.to_string()) {
+            if is_start_tag(tag_or_content.to_string()) {
+                scope.push(get_tag_name(tag_or_content.to_string()));
+                current_parent.push(RmlElement::new(get_tag_name(tag_or_content.to_string())));
+                tags.iter().for_each(|tag| current_parent.last_mut().unwrap().add_tag(parse_tag_from_string(tag.to_string())));
+                current_parent_names.push(get_tag_name(tag_or_content.to_string()));
+            } else if is_end_tag(tag_or_content.to_string()) {
+                let index = scope.iter().position(|element| *element==*get_tag_name(tag_or_content.to_string()));
+                scope.remove(index.unwrap());
+                if child_hashmap.contains_key(&get_tag_name(tag_or_content.to_string())) {
+                    child_hashmap.get(&get_tag_name(tag_or_content.to_string())).expect("tag_or_content doesnt exist in child_hashmap").iter().for_each(|child| {
+                    current_parent.get_mut(get_index(current_parent_names.clone(), get_tag_name(tag_or_content.to_string()))).unwrap().add_child(child.clone());
+                        
+                    });
+                }
+                if !scope.is_empty() {
+                    child_hashmap.entry(scope.last().unwrap().to_string()).or_insert_with(|| {Vec::new()}).push(current_parent.iter().last().unwrap().clone());
+
+                }
+            }
+            
+
+        } else {
+            current_parent.last_mut().unwrap().add_tag(RmlTag { name: "*content*".to_string(), value: tag_or_content.to_string() });
+        }
+    });
+    current_parent.first().unwrap().clone()
+}
 
 #[cfg(test)]
 mod tests {
