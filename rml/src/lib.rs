ed6d77a0b04ae86d9e37dd242f39172ffc49bac4
DevThatCodes <cupcooklinux@gmail.com>
2024-10-27 10:09:50 -0400

    added how the parser will work

--- rml/src/lib.rs
+++ rml/src/lib.rs
@@ -34,7 +34,7 @@ impl RmlElement {
     // returns a reference to the children field
     pub fn get_children(&self) -> Vec<&RmlElement> {
         let mut output : Vec<&RmlElement> = Vec::new();
-        let _ = &self.children.iter().for_each(|child| output.push(child));
+        self.children.iter().for_each(|child| output.push(child));
         output
     }
     // returns name as a string
@@ -45,6 +45,11 @@ impl RmlElement {
     pub fn get_tags(&self) -> &Vec<RmlTag> {
         &self.tags
     }
+
+    pub fn add_tag(&mut self, tag: RmlTag) {
+        self.tags.push(tag);
+    }
+
     // adds a child to the RmlElement
     pub fn add_child(&mut self, element: RmlElement) {
         let _ = &self.children.push(Box::new(element));
@@ -68,10 +73,23 @@ impl RmlTag {
     }
 }
 
-// TODO: make a parser
+// TODO: 2, 3.
+// DONE: 1.
 // 1. Figure out the algorithm to do it.
 // 2. Pseudocode it.
 // 3. Translate pseudocode to rust code.
+//
+// How it will work:
+// finds a start tag and adds that to the scope and adds the key to the child hashmap,
+// puts all content in the child hashmap with the key as the most recently added tag to the scope,
+// then once the end tag of the most recently added tag to the scope is found, the children and
+// content is added to the element and either added to the key in the child hashmap with the next
+// tag in the scope, or returns the element.
+//
+// Pseudocode:
+// parse_from_string(string data) {
+//  
+// }
 
 #[cfg(test)]
 mod tests {
@@ -92,5 +110,17 @@ mod tests {
             name: "DefaultTag".to_string(),
             value: "DefaultValue".to_string()
         })
+    }
+
+    #[test]
+    fn test_add_tag() {
+        let mut default_element_added_tag = RmlElement::default();
+        default_element_added_tag.add_tag(RmlTag::default());
+        let element_with_tag = RmlElement {
+            name: "DefaultElement".to_string(),
+            children: Vec::new(),
+            tags: vec![RmlTag::default()]
+        };
+        assert_eq!(default_element_added_tag, element_with_tag);
     }
 }
